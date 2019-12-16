97ae7c0f72e0b51952b2e81b067e42aefca21333
L31T40 <jorgeleitao76@gmail.com>
2019-12-16 22:37:14 +0000

    inicio

---
+++ store/resources/views/home/app.blade.php
@@ -0,0 +1,29 @@
+@section(content)
+<div class="container-web">
+            <div class="container">
+                <div class="row list-logo-top-v3">
+                    <ul class="clear-margin border-collapsed-box">
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_3.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_7.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_4.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_8.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_5.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_9.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_6.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_10.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_1.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_11.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_2.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_12.png" alt="Logo" /></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_13-min.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_14-min.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_4.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_15-min.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_16-min.png" alt="Logo"></a></li>
+                        <li class="border-collapsed-element"><a href="#"><img src="img/logo_17-min.png" alt="Logo"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        @endsection
---
+++ store/resources/views/layout/leftnavbar.blade.php
@@ -0,0 +1,20 @@
+
+@section('leftnavbar')
+<div class="clearfix menu_more_header menu-web menu-bg-white">
+            <ul>
+                <li><a href="#"><img src="img/icon_hot_gray.png" alt="Icon Hot Deals" /> <p>Hot Deals</p></a></li>
+                <li><a href="#"><img src="img/icon_food_gray.png" alt="Icon Food" /> <p>Food</p></a></li>
+                <li><a href="#"><img src="img/icon_mobile_gray.png" alt="Icon Mobile & Tablet" /> <p>Mobile & Tablet</p></a></li>
+                <li><a href="#"><img src="img/icon_electric_gray.png" alt="Icon Electric Appliances" /> <p>Electric Appliances</p></a></li>
+                <li><a href="#"><img src="img/icon_computer_gray.png" alt="Icon Electronics & Technology" /> <p>Electronics & Technology</p></a></li>
+                <li><a href="#"><img src="img/icon_fashion_gray.png" alt="Icon Fashion" /> <p>Fashion</p></a></li>
+                <li><a href="#"><img src="img/icon_health_gray.png" alt="Icon Health & Beauty" /> <p>Health & Beauty</p></a></li>
+                <li><a href="#"><img src="img/icon_mother_gray.png" alt="Icon Mother & Baby" /> <p>Mother & Baby</p></a></li>
+                <li><a href="#"><img src="img/icon_book_gray.png" alt="Icon Books & Stationery" /> <p>Books & Stationery</p></a></li>
+                <li><a href="#"><img src="img/icon_home_gray.png" alt="Icon Home & Life" /> <p>Home & Life</p></a></li>
+                <li><a href="#"><img src="img/icon_sport_gray.png" alt="Icon Sports & Outdoors" /> <p>Sports & Outdoors</p></a></li>
+                <li><a href="#"><img src="img/icon_auto_gray.png" alt="Icon Auto & Moto" /> <p>Auto & Moto</p></a></li>
+                <li><a href="#"><img src="img/icon_voucher_gray.png" alt="Icon Voucher Service" /> <p>Voucher Service</p></a></li>
+            </ul>
+        </div> 
+@endsection
